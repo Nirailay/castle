f4da0668a669884b5504790d663bd8bb6f32eb54
Niko <Nirailay@gmail.com>
2019-01-22 15:06:15 +0100

    <<En cours 1

---
+++ potusScraper.js
@@ -0,0 +1,10 @@
+var rp = require('request-promise');
+var $ = require('cheerio');
+var url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+
+rp(url)
+  .then(function(html){
+    //success!
+    console.log($('big > a', html).length);
+    console.log($('big > a', html));
+  });
